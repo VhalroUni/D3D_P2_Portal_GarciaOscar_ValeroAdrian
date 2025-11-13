9429864813fa0201d9eccbfbcd9f4443a29f45ed
VhalroUni <avalerof@edu.tecnocampus.cat>
2025-11-13 22:02:36 +0100

    mas mapa

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -17,8 +17,8 @@ public class Portal : MonoBehaviour
     public void LateUpdate()
     {
         Vector3 l_WorldPosition = Camera.main.transform.position;
-        Vector3 l_LocalPosition = m_OtherPortalTransform.InverseTransformPoint(l_WorldPosition);
-        m_MirrorPortal.m_Camera.transform.position = m_MirrorPortal.transform.TransformPoint(l_LocalPosition);
+        Vector3 l_LocalPosition = m_OtherPortalTransform.TransformPoint(l_WorldPosition);
+        m_MirrorPortal.m_Camera.transform.position = m_MirrorPortal.transform.InverseTransformPoint(l_LocalPosition);
 
         Vector3 l_WorldForward = Camera.main.transform.forward;
         Vector3 l_LocalForward = m_OtherPortalTransform.InverseTransformDirection(l_WorldForward);
